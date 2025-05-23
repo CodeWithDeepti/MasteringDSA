75fca99e786a0d48f27127c2635beabfce451177
Deepti Arora <deeepti.arora@gmail.com>
2025-05-23 13:46:41 +0530

    Added program to find second largest element

--- src/main/java/one/six/zero/days/dsa/arrays/Day1SecondLargest.java
+++ src/main/java/one/six/zero/days/dsa/arrays/Day1SecondLargest.java
@@ -1,10 +1,33 @@
 package one.six.zero.days.dsa.arrays;
 
+/*Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+*Constraints:
+2 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+*/
+
 public class Day1SecondLargest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public int getSecondLargest(int[] arr) {
+        int largest = arr[0];
+        int secondLargest = -1;
+        for(int value : arr){
+            if(value > largest){
+                secondLargest=largest;
+                largest=value;
+            }else if(value < largest && value>secondLargest){
+                secondLargest=value;
+            }
+        }
+        return secondLargest;   
+    }
 
 }
