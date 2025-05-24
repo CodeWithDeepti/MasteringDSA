fafce1a15dc6359db2917efdd4efa46319883621
Deepti Arora <deeepti.arora@gmail.com>
2025-05-24 12:31:01 +0530

    added comments for buy and sell stock in which multiple transaction are allowed

--- src/main/java/one/six/zero/days/dsa/arrays/Day7StockBuyandSellMultipleTransactionAllowed.java
+++ src/main/java/one/six/zero/days/dsa/arrays/Day7StockBuyandSellMultipleTransactionAllowed.java
@@ -1,5 +1,16 @@
 package one.six.zero.days.dsa.arrays;
 
+/*
+ * The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock i at price[i], you can even buy and sell the stock on the same day.
+ * Find the maximum profit that you can get.
+
+Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
+
+Constraints:
+1 <= prices.size() <= 105
+0 <= prices[i] <= 104
+
+ */
 public class Day7StockBuyandSellMultipleTransactionAllowed {
 
 	public static void main(String[] args) {
